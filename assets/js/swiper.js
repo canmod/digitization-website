53f3290e6c2af292479fceb2febfaf6ab7a69c57
ronaldLjin <ronyjin123@gmail.com>
2022-07-14 13:01:13 -0600

    new banner design

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -58,3 +58,25 @@ const swiper2 = new Swiper(".swiper2", {
     swiper: swiper2Thumb,
   },
 });
+
+const swiper3Thumb = new Swiper(".swiper3-thumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+const swiper3 = new Swiper(".swiper3", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  thumbs: {
+    swiper: swiper3Thumb,
+  },
+});
